23ad49860ae3041fb435b9ad1116f07ab764a8ce
Pranav Kumar <pranav@wecallmedia.com>
2023-04-07 23:18:28 +0530

    Added Loader

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import { useStoryblok, StoryblokComponent } from "@storyblok/react";
 import Prelander from "./PreLander";
-import PropagateLoader from "react-spinners/PropagateLoader"
+import PuffLoader from "react-spinners/PuffLoader"
 import "components/GlobalCss";
 import Lander from "./Lander";
 
@@ -62,7 +62,10 @@ const App = () => {
           alignItems: "center",
         }}
       >
-        <PropagateLoader color="#36d7b7" />
+        <div>
+        <PuffLoader color="#274066" />
+        <p style={{ margin: 0, padding: 0 }}>Loading...</p>
+        </div>
       </div>
     );
   }
