07713010c86a41f125843c7fa76e603fb37eb54e
Pranav Kumar <pranav@wecallmedia.com>
2023-04-29 02:09:15 +0530

    Bug Fix

--- src/html/scripts.js
+++ src/html/scripts.js
@@ -213,7 +213,7 @@ const domain_settings = [
             title: "Meta title",
             description: "Meta Description",
         },
-        tikTok: "",
+        tikTok: "CH4QHMRC77UBPA5M9M80",
         industry: "",
         websiteTitle: "Localhost",
         vertical: "medicareaca",
