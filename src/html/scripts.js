a21e7e3432963e3937333af6a06853c9b3dbcf10
praispranav <praispranav@gmail.com>
2023-07-31 21:27:51 +0530

    Added timeforacahelp.com

--- src/html/scripts.js
+++ src/html/scripts.js
@@ -692,6 +692,29 @@ const domain_settings = [
         vertical: "aca",
         storyblockAccessToken: "tWQ76Q1L6VnCWJbGxPr4Jwtt",
     },
+    {
+        name: "prelander.timeforacahelp.com",
+        gtm: "GTM-TJ9MVL9",
+        facebookPixel: "632024732266157",
+        volum: {
+            url: "https://track.americanassurance.org/d/.js",
+            noScript:
+                "https://track.americanassurance.org/d/.js?noscript=true&lpurl=",
+            meta: "https://track.americanassurance.org",
+        },
+        metaDetails: {
+            "og:title": "og title",
+            "og:description": "og description",
+            "og:site_name": "og site_name",
+            title: "Meta title",
+            description: "Meta Description",
+        },
+        tikTok: "",
+        industry: "",
+        websiteTitle: "",
+        vertical: "aca",
+        storyblockAccessToken: "jPS1PqaLIXgBQnzqtjQRAAtt",
+    },
 ];
 
 initScripts(domain_settings);
