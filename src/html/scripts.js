3b442e24e7baec8afd86a6a2a2660b54f1f26515
praispranav <praispranav@gmail.com>
2023-10-04 01:30:36 +0530

    Added subsidyacrossnation.com

--- src/html/scripts.js
+++ src/html/scripts.js
@@ -322,8 +322,6 @@ const domain_settings = [
         vertical: "medicareaca",
         storyblockAccessToken: "gVJgZvajxLWDT0saMgTqswtt",
     },
-
-
     {
         name: "prelander.qualifyaca.com",
         gtm: "GTM-M8F8CW4",
@@ -829,6 +827,29 @@ const domain_settings = [
         websiteTitle: "",
         vertical: "aca",
         storyblockAccessToken: "hrN1KIrdox1RnNAu1XQQCwtt",
+    },
+       {
+        name: "prelander.subsidyacrossnation.com",
+        gtm: "GTM-M8F8CW4",
+        facebookPixel: "3630004413952856",
+        volum: {
+            url: "https://track.subsidyacrossnation.com/d/.js",
+            noScript:
+                "https://track.subsidyacrossnation.com/d/.js?noscript=true&lpurl=",
+            meta: "https://track.subsidyacrossnation.com",
+        },
+        metaDetails: {
+            "og:title": "og title",
+            "og:description": "og description",
+            "og:site_name": "og site_name",
+            title: "Meta title",
+            description: "Meta Description",
+        },
+        tikTok: "",
+        industry: "",
+        websiteTitle: "",
+        vertical: "aca",
+        storyblockAccessToken: "RuU8GjUyc1ufhWtGwVQOzgtt",
     },
 ];
 
