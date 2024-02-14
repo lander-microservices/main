3aa7842103291b69efdd67ad8d15854c52e717ea
praispranav <praispranav@gmail.com>
2024-02-14 06:10:18 -0500

    Removed Clocking and Enabled Config file

---
+++ webpack.noclocking.config.js
@@ -0,0 +1,98 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+require('dotenv').config();
+
+const fs = require('fs');
+try{
+  fs.mkdirSync('./dist');
+} catch(error){}
+fs.copyFileSync('./src/_redirects', "./dist/_redirects");
+
+
+const files = fs.readdirSync('./src/html/favicon');
+const arr = Array.from(files);
+arr.forEach((i)=> {
+  fs.copyFileSync(`./src/html/favicon/${i}`, `./dist/${i}`);
+})
+
+fs.copyFileSync('./src/html/scripts.js', "./dist/scripts.js");;
+fs.copyFileSync('./src/html/volumLanderScript.js', "./dist/volumLanderScript.js");;
+fs.copyFileSync('./src/html/volumOfferScript.js', "./dist/volumOfferScript.js");;
+
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    // publicPath: "https://illustrious-squirrel-bec40c.netlify.app/",
+    publicPath: "https://lander-main-microservice.netlify.app/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "main",
+      filename: "remoteEntry.js",
+      remotes: {
+        landers: "lander@https://lander-component-microservice.netlify.app/remoteEntry.js",
+        prelander: "prelander@https://preladner-micorservice.netlify.app/remoteEntry.js",
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new CompressionPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/html/" + 'index' + '.html',
+    }),
+  ],
+};
