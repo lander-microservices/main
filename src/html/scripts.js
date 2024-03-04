a2c31074ed711487dddeb84734c4157fa2481f56
sahilstm <154385778+sahilstm@users.noreply.github.com>
2024-03-05 04:00:33 +0530

    added prohealthy point domains

--- src/html/scripts.js
+++ src/html/scripts.js
@@ -1258,6 +1258,29 @@ const domain_settings = [
         vertical: "aca",
         storyblockAccessToken: "cXtgCL4pDo8Ggl2wwVXAwgtt",
       },
+      {
+        name: "prelandertest.prohealthypoint.com",
+        gtm: "GTM-PHDTN6NR",
+        facebookPixel: "687474176447071",
+        volum: {
+          url: "https://track.prohealthypoint.com/d/.js",
+          noScript:
+            "https://track.prohealthypoint.com/d/.js?noscript=true&lpurl=",
+          meta: "https://track.prohealthypoint.com",
+        },
+        metaDetails: {
+          "og:title": "og title",
+          "og:description": "og description",
+          "og:site_name": "og site_name",
+          title: "Meta title",
+          description: "Meta Description",
+        },
+        tikTok: "",
+        industry: "",
+        websiteTitle: "Pro Healthy Point",
+        vertical: "aca",
+        storyblockAccessToken: "r7geRrDfcqumatSMWrplaQtt",
+      },
 ];
 
 initScripts(domain_settings);
